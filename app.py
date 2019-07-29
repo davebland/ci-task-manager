d2a92951bfb72e9cf99b2903bf0eea00af68df50
Ubuntu <ubuntu@ip-172-31-19-78.eu-west-1.compute.internal>
2019-07-29 13:49:35 +0000

    basic app setup and mongodb connection

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+import json
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+# Get creds from untracked file
+with open('mongo_creds.txt') as creds:
+    data = json.load(creds)
+    app.config['MONGO_DBNAME'] = data['MONGO_DBNAME']
+    app.config['MONGO_URI'] = data['MONGO_URI']
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
